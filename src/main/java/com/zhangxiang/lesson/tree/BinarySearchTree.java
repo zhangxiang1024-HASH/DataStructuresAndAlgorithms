ca9316f7587f34b499a1a17630aa5b868019390b
张祥 <zhangxiang02@edaijia-inc.cn>
2022-02-10 19:59:34 +0800

    数据结构与算法课程：二叉查找树

---
+++ src/main/java/com/zhangxiang/lesson/tree/BinarySearchTree.java
@@ -0,0 +1,265 @@
+package com.zhangxiang.lesson.tree;
+
+import com.zhangxiang.lesson.tree.printer.BinaryTreeInfo;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author: zhangxiang
+ * @createTime: 2022年02月09日 11:36:02
+ * @desc: 二叉查找树
+ */
+public class BinarySearchTree<E> implements BinaryTreeInfo {
+    private int size;
+    private Node<E> root;
+    private Comparator<E> comparator; //比较元素大小的规则
+
+    public BinarySearchTree() {
+        this(null);
+    }
+
+    public BinarySearchTree(Comparator<E> comparator) {
+        this.comparator = comparator;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public void add(E element) {
+        elementNotNullCheck(element);
+        if (root == null) {
+            root = new Node<>(element, null);
+            size++;
+            return;
+        }
+        Node<E> parent = root;
+        Node<E> node = root;
+        int compare = 0;
+        while (node != null) {
+            compare = compare(element, node.element);
+            parent = node;
+            if (compare > 0) {
+                node = node.right;
+            } else if (compare < 0) {
+                node = node.left;
+            } else {//相等
+                return;
+            }
+        }
+        Node<E> newNode = new Node<>(element, parent);
+        if (compare > 0) {
+            parent.right = newNode;
+        } else {
+            parent.left = newNode;
+        }
+        size++;
+    }
+
+    private int compare(E element1, E element2) {
+        if (comparator != null) {
+            return comparator.compare(element1, element2);
+        }
+        return ((Comparable<E>) element1).compareTo(element2);
+    }
+
+    public void remove(E element) {
+
+    }
+
+    public boolean contains(E Element) {
+        return false;
+    }
+
+    private void elementNotNullCheck(E element) {
+        if (element == null) {
+            throw new IllegalArgumentException("element must be not null");
+        }
+    }
+
+    /**
+     * 用于图形打印树的各个节点
+     *
+     * @return
+     */
+    @Override
+    public Object root() {
+        return root;
+    }
+
+    @Override
+    public Object left(Object node) {
+        return ((Node<E>) node).left;
+    }
+
+    @Override
+    public Object right(Object node) {
+        return ((Node<E>) node).right;
+    }
+
+    @Override
+    public Object string(Object node) {
+        return ((Node<E>) node).element;
+    }
+
+    public static class Node<E> {
+        public E element;
+        public Node<E> left;
+        public Node<E> right;
+        public Node<E> parent;
+
+        public Node(E element, Node<E> parent) {
+            this.element = element;
+            this.parent = parent;
+        }
+    }
+
+    /**
+     * 前序遍历: 根节点、左子树、右子树
+     */
+    public void preorderTraversal(Visitor<E> visitor) {
+        if (visitor == null) {
+            return;
+        }
+        preorderTraversal(root, visitor);
+    }
+
+    private void preorderTraversal(Node<E> node, Visitor<E> visitor) {
+        if (node == null) {
+            return;
+        }
+        visitor.visit(node);
+        preorderTraversal(node.left, visitor);
+        preorderTraversal(node.right, visitor);
+    }
+
+    /**
+     * 中序遍历：左子树、根节点、右子树
+     */
+    public void inorderTraversal(Visitor<E> visitor) {
+        if (visitor == null) {
+            return;
+        }
+        inorderTraversal(root, visitor);
+    }
+
+    private void inorderTraversal(Node<E> node, Visitor<E> visitor) {
+        if (node == null) {
+            return;
+        }
+        inorderTraversal(node.left, visitor);
+        visitor.visit(node);
+        inorderTraversal(node.right, visitor);
+    }
+
+    /**
+     * 后序遍历：左子树、右子树、根节点
+     */
+    public void postorderTraversal(Visitor<E> visitor) {
+        if (visitor == null) {
+            return;
+        }
+        postorderTraversal(root, visitor);
+    }
+
+    private void postorderTraversal(Node<E> node, Visitor<E> visitor) {
+        if (node == null) {
+            return;
+        }
+        postorderTraversal(node.left, visitor);
+        postorderTraversal(node.right, visitor);
+        visitor.visit(node);
+    }
+
+    /**
+     * 层序遍历
+     */
+    public void levelOrderTraversal(Visitor<E> visitor) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            Node<E> node = queue.poll();
+            visitor.visit(node);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface Visitor<E> {
+        void visit(Node<E> node);
+    }
+
+    /**
+     * 求高度
+     *
+     * @return
+     */
+    public int height() {
+        //递归
+        //return heightRecursive(root);
+        //非递归
+        return height(root);
+    }
+
+    /**
+     * 递归求高度
+     *
+     * @param node
+     * @return
+     */
+    private int heightRecursive(Node<E> node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(heightRecursive(node.left), heightRecursive(node.right));
+    }
+
+    /**
+     * 非递归(利用层序遍历)求高度
+     *
+     * @param node
+     * @return
+     */
+    private int height(Node<E> node) {
+        if (node == null) {
+            return 0;
+        }
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.offer(node);
+        int levelSize = 1;//记录每一层节点的个数 默认为1
+        int height = 0;
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            levelSize--;
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+            if (levelSize == 0) {
+                levelSize = queue.size();
+                height++;
+            }
+        }
+        return height;
+    }
+}
---
+++ src/main/java/com/zhangxiang/lesson/tree/main/BinarySearchTreeMain.java
@@ -0,0 +1,36 @@
+package com.zhangxiang.lesson.tree.main;
+
+import com.zhangxiang.lesson.tree.BinarySearchTree;
+import com.zhangxiang.lesson.tree.printer.BinaryTrees;
+
+/**
+ * @author: zhangxiang
+ * @createTime: 2022年02月10日 16:29:09
+ * @desc:
+ */
+public class BinarySearchTreeMain {
+    public static void main(String[] args) {
+        Integer[] data = new Integer[]{7,4,9,2,5,8,11,3};
+        BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
+        /*BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.intValue() - o1.intValue();
+            }
+        });*/
+        for (int i = 0; i < data.length; i++) {
+            binarySearchTree.add(data[i]);
+        }
+        BinaryTrees.println(binarySearchTree);
+        System.out.println("前序遍历:");
+        binarySearchTree.preorderTraversal(node -> System.out.println(node.element));
+        System.out.println("中序遍历:");
+        binarySearchTree.inorderTraversal(node -> System.out.println(node.element));
+        System.out.println("后序遍历:");
+        binarySearchTree.postorderTraversal(node -> System.out.println(node.element));
+        System.out.println("层序遍历:");
+        binarySearchTree.levelOrderTraversal(node -> System.out.println(node.element));
+
+        System.out.println(binarySearchTree.height());
+    }
+}

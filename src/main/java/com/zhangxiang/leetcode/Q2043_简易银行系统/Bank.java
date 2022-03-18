4613dafc55cda8bd69c9b2287c35979725f396d0
张祥 <zhangxiang02@edaijia-inc.cn>
2022-03-18 19:47:18 +0800

    leetcode

---
+++ src/main/java/com/zhangxiang/leetcode/Q2043_简易银行系统/Bank.java
@@ -0,0 +1,39 @@
+package com.zhangxiang.leetcode.Q2043_简易银行系统;
+
+/**
+ * @author: zhangxiang
+ * @createTime: 2022年03月18日 19:28:08
+ * @desc: https://leetcode-cn.com/problems/simple-bank-system/
+ */
+public class Bank {
+    long[] balance;
+
+    public Bank(long[] balance) {
+        this.balance = balance;
+    }
+
+    public boolean transfer(int account1, int account2, long money) {
+        if (account1 > balance.length || account2 > balance.length || balance[account1 - 1] < money) {
+            return false;
+        }
+        balance[account1 - 1] -= money;
+        balance[account2 - 1] += money;
+        return true;
+    }
+
+    public boolean deposit(int account, long money) {
+        if (account > balance.length) {
+            return false;
+        }
+        balance[account - 1] += money;
+        return true;
+    }
+
+    public boolean withdraw(int account, long money) {
+        if (account > balance.length || balance[account - 1] < money) {
+            return false;
+        }
+        balance[account - 1] -= money;
+        return true;
+    }
+}

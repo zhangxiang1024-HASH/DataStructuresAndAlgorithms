37fc31d60edad93d61fae3abcf9930f922dd23a0
张祥 <zhangxiang02@edaijia-inc.cn>
2022-04-12 21:00:53 +0800

    插入排序 用二分查找优化

--- src/main/java/com/zhangxiang/lesson/sorting/InsertionSort.java
+++ src/main/java/com/zhangxiang/lesson/sorting/InsertionSort.java
@@ -8,8 +8,7 @@ import java.util.Arrays;
  * @desc: 插入排序
  */
 public class InsertionSort<E extends Comparable<E>> extends Sort<E>{
-    @Override
-    protected void sort() {
+    protected void sort2() {
         for (int begin = 1; begin < array.length; begin++) {
             int cur = begin;
             E e = array[cur];
@@ -19,5 +18,37 @@ public class InsertionSort<E extends Comparable<E>> extends Sort<E>{
             }
             array[cur] = e;
         }
+    }
+
+    /**
+     * 用二分查找优化
+     */
+    @Override
+    protected void sort() {
+        for (int begin = 1; begin < array.length; begin++) {
+            E e = array[begin];
+            int swapIndex = binarySearch(begin, e);
+            for (int i = begin -1; i >= swapIndex ; i--) {
+                array[i+1] = array[i];
+            }
+            array[swapIndex] = e;
+        }
+    }
+
+    private int binarySearch(int begin,E e) {
+        int binaryBegin = 0;
+        int binaryEnd = begin;
+        while (binaryBegin < binaryEnd){
+            int mid = (binaryBegin + binaryEnd) >> 1;
+            boolean b = cmp(e, array[mid]) < 0;
+            if(mid == 0 || (b && cmp(e,array[mid -1]) > 0)){
+                return mid;
+            }else if(b){
+                binaryEnd = mid;
+            }else {
+                binaryBegin = mid + 1;
+            }
+        }
+        return 0;
     }
 }
